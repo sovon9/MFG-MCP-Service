d3879ddfdc1afc37cbf7d2acfd54733404f15f6d
sovon singha <sovonsingha9@gmail.com>
2025-12-10 21:58:40 +0530

    remote mcp

--- src/main/java/com/sovon9/MCPServer/tool/GithubTool.java
+++ src/main/java/com/sovon9/MCPServer/tool/GithubTool.java
@@ -1,14 +1,13 @@
 package com.sovon9.MCPServer.tool;
 
 import org.springaicommunity.mcp.annotation.McpTool;
-import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GithubTool {
 
     //tools
-    @Tool(name = "sovon-github", description = "return sovon github link")
+    @McpTool(name = "sovon-github", description = "return sovon github link")
     public String sovonGithubLink()
     {
         String link = """
